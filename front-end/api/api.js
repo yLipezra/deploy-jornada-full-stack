b00509f1131307fc4dd98d4203ac379b92f93e50
Diego <fullstackjornada@gmail.com>
2025-02-14 01:37:28 -0300

    ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,8 +2,9 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+// const { NODE_ENV } = process.env;
+// const URL = "http://localhost:3001/api";
+const URL = "https://deploy-jornada-full-stack.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
